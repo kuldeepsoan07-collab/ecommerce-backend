d5d74aefa2110b3bf375ecf855f1f9fc8a98e828
kuldeepsoan07-collab <kuldeepsoan.07@gmail.com>
2026-09-07 19:33:36 +0530

    Fix production CORS

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,6 @@
-const API_URL = "http://localhost:3000/api";
+const API_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:3000/api";
+
 
 export const loginUser = async (userData) => {
   const response = await fetch(`${API_URL}/auth/login`, {
--- src/app.js
+++ src/app.js
@@ -8,7 +8,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://ecommerce-frontend-rust-kappa.vercel.app",
+    ],
     credentials: true,
   })
 );
