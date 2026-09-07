a4516e771986fc65768506a7f71395bbf7b113a7
kuldeepsoan07-collab <kuldeepsoan.07@gmail.com>
2026-09-07 19:42:01 +0530

    Fix Gmail SMTP connection

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -15,7 +15,9 @@ oauth2Client.setCredentials({
 });
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
 
   auth: {
     type: "OAuth2",
