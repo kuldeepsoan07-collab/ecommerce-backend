6d5de5580ca65793f5eacd1db56dc2382e5c8857
kuldeepsoan07-collab <kuldeepsoan.07@gmail.com>
2026-09-09 12:01:30 +0530

    Replace Gmail SMTP with Resend

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,62 +1,25 @@
-import nodemailer from "nodemailer";
-import { google } from "googleapis";
-import config from "../config/config.js";
+import { Resend } from "resend";
 
-const OAuth2 = google.auth.OAuth2;
-
-const oauth2Client = new OAuth2(
-  config.GOOGLE_CLIENT_ID,
-  config.GOOGLE_CLIENT_SECRET,
-  "https://developers.google.com/oauthplayground"
-);
-
-oauth2Client.setCredentials({
-  refresh_token: config.GOOGLE_REFRESH_TOKEN,
-});
-
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-
-  auth: {
-    type: "OAuth2",
-    user: config.GOOGLE_USER,
-    clientId: config.GOOGLE_CLIENT_ID,
-    clientSecret: config.GOOGLE_CLIENT_SECRET,
-    refreshToken: config.GOOGLE_REFRESH_TOKEN,
-  },
-});
-
-transporter.verify((error, success) => {
-  if (error) {
-    console.error("Email connection error:", error);
-  } else {
-    console.log("Email server is ready");
-  }
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (to, subject, text, html) => {
   try {
-    const accessToken = await oauth2Client.getAccessToken();
-
-    transporter.set("oauth2", {
-      user: config.GOOGLE_USER,
-      accessToken: accessToken.token,
-    });
-
-    const info = await transporter.sendMail({
-      from: `"Your E-Commerce" <${config.GOOGLE_USER}>`,
-      to,
+    const { data, error } = await resend.emails.send({
+      from: "Your E-Commerce <onboarding@resend.dev>",
+      to: [to],
       subject,
       text,
       html,
     });
 
-    console.log("Email sent:", info.messageId);
+    if (error) {
+      console.error("Resend email error:", error);
+      throw new Error(error.message);
+    }
 
-    return info;
+    console.log("Email sent:", data?.id);
 
+    return data;
   } catch (error) {
     console.error("Error sending email:", error);
     throw error;
